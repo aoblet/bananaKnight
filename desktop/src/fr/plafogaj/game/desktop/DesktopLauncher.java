34503ccb6f317af9a79d89cae264f230ae85c548
Alex <alexis.oblet@gmail.com>
2015-04-17 17:30:46 +0200

    init commit

---
+++ desktop/src/fr/plafogaj/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.plafogaj.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.plafogaj.game.BananaKnight;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BananaKnight(), config);
+	}
+}
---
+++ html/src/fr/plafogaj/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package fr.plafogaj.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import fr.plafogaj.game.BananaKnight;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new BananaKnight();
+        }
+}
