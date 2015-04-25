3e7d796e2e1c1a48eb35b5c17570e4a498ac774f
Alex <alexis.oblet@gmail.com>
2015-04-25 17:05:02 +0200

    tiledMap gameLevel config

---
+++ core/src/fr/plafogaj/game/level/GameLevelConfig.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2015 see AUTHORS file
+ * This file is part of BananaKnight.
+ * BananaKnight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * BananaKnight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with BananaKnight.  If not, see <http://www.gnu.org/licenses/>
+ */
+package fr.plafogaj.game.level;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+
+public class GameLevelConfig {
+    private FileHandle m_levelFile;
+    private FileHandle m_mapTmxFile;
+    private FileHandle m_configGameFile;
+
+    private JsonValue m_charactersConfig;
+    private JsonValue m_artefactsConfig;
+
+    private TiledMapConfig m_tileMapConfig;
+
+    public GameLevelConfig(FileHandle levelFile) throws NullPointerException{
+        this.setUpConfig(levelFile);
+    }
+
+    private void setUpConfig(FileHandle levelFile) throws NullPointerException{
+        JsonValue root = new JsonReader().parse(m_levelFile = levelFile);
+        m_mapTmxFile = Gdx.files.internal(root.get("mapTmxFile").asString());
+        m_configGameFile = Gdx.files.internal(root.get("configGameFile").asString());
+
+        JsonValue rootConfig = new JsonReader().parse(m_configGameFile);
+        m_charactersConfig = rootConfig.get("charactersConfig");
+        m_artefactsConfig = rootConfig.get("artefactsConfig");
+
+        m_tileMapConfig = new TiledMapConfig(m_mapTmxFile.path());
+    }
+
+    public TiledMapConfig getTileMapConfig() {
+        return m_tileMapConfig;
+    }
+
+    public FileHandle getLevelFile() {
+        return m_levelFile;
+    }
+
+    public FileHandle getMapTmxFile() {
+        return m_mapTmxFile;
+    }
+
+    public FileHandle getConfigGameFile() {
+        return m_configGameFile;
+    }
+
+    public JsonValue getCharactersConfig() {
+        return m_charactersConfig;
+    }
+
+    public JsonValue getArtefactsConfig() {
+        return m_artefactsConfig;
+    }
+}
---
+++ core/src/fr/plafogaj/game/level/TiledMapConfig.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright 2015 see AUTHORS file
+ * This file is part of BananaKnight.
+ * BananaKnight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * BananaKnight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with BananaKnight.  If not, see <http://www.gnu.org/licenses/>
+ */
+package fr.plafogaj.game.level;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+/**
+ * Class which facilites use and access of TiledMap
+ * Contains the different tiledMapsLayer common to every level
+ */
+public class TiledMapConfig{
+    private TiledMap m_map;
+    private TiledMapTileLayer m_backgroundFarLayer;
+    private TiledMapTileLayer m_backgroundNearLayer;
+    private TiledMapTileLayer m_foregroundLayer;
+    /**
+     * could be related to decor
+     */
+    private TiledMapTileLayer m_collisionLayer;
+
+    private float m_tileUnitScale;
+    private OrthogonalTiledMapRenderer m_backgroundFarLayerRenderer;
+    private OrthogonalTiledMapRenderer m_backgroundNearLayerRenderer;
+    private OrthogonalTiledMapRenderer m_foregroundLayerRenderer;
+    private OrthogonalTiledMapRenderer m_collisionLayerRenderer;
+
+    private OrthographicCamera m_camera;
+
+    public TiledMapConfig(String tmxFile){
+        m_map = new TmxMapLoader().load(tmxFile);
+        m_tileUnitScale = 1/32f;
+
+        m_backgroundFarLayer = (TiledMapTileLayer)m_map.getLayers().get("backgroundFarLayer");
+        m_backgroundNearLayer = (TiledMapTileLayer)m_map.getLayers().get("backgroundNearLayer");
+        m_collisionLayer = (TiledMapTileLayer)m_map.getLayers().get("collisionLayer");
+        m_foregroundLayer = (TiledMapTileLayer)m_map.getLayers().get("foregroundLayer");
+
+        m_collisionLayerRenderer = new OrthogonalTiledMapRenderer(m_map, m_tileUnitScale);
+        m_backgroundFarLayerRenderer = new OrthogonalTiledMapRenderer(m_map, m_tileUnitScale);
+        m_backgroundNearLayerRenderer = new OrthogonalTiledMapRenderer(m_map, m_tileUnitScale);
+        m_foregroundLayerRenderer = new OrthogonalTiledMapRenderer(m_map, m_tileUnitScale);
+
+        m_camera = new OrthographicCamera();
+        m_camera.setToOrtho(false, 40, 20);
+        m_camera.update();
+    }
+
+    public float getTileUnitScale() {
+        return m_tileUnitScale;
+    }
+
+    public TiledMap getMap() {
+        return m_map;
+    }
+
+    public TiledMapTileLayer getBackgroundFarLayer() {
+        return m_backgroundFarLayer;
+    }
+
+    public TiledMapTileLayer getBackgroundNearLayer() {
+        return m_backgroundNearLayer;
+    }
+
+    public TiledMapTileLayer getCollisionLayer() {
+        return m_collisionLayer;
+    }
+
+    public TiledMapTileLayer getForegroundLayer() {
+        return m_foregroundLayer;
+    }
+
+    public OrthogonalTiledMapRenderer getBackgroundFarLayerRenderer() {
+        return m_backgroundFarLayerRenderer;
+    }
+
+    public OrthogonalTiledMapRenderer getBackgroundNearLayerRenderer() {
+        return m_backgroundNearLayerRenderer;
+    }
+
+    public OrthogonalTiledMapRenderer getForegroundLayerRenderer() {
+        return m_foregroundLayerRenderer;
+    }
+
+    public OrthogonalTiledMapRenderer getCollisionLayerRenderer() {
+        return m_collisionLayerRenderer;
+    }
+
+    public OrthographicCamera getCamera() {
+        return m_camera;
+    }
+}
