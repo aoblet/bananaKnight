9141abd534acf8fbe74502b1cbfed63ab2473b87
Alex <alexis.oblet@gmail.com>
2015-04-30 01:23:05 +0200

    player

--- core/src/fr/plafogaj/game/character/Character.java
+++ core/src/fr/plafogaj/game/character/Character.java
@@ -15,15 +15,20 @@
 
 package fr.plafogaj.game.character;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import fr.plafogaj.game.character.animation.SpriteTextureConfig;
+import fr.plafogaj.game.engine.TiledMapConfig;
+import fr.plafogaj.game.engine.TiledMapOrthographicCamera;
 import fr.plafogaj.game.weapon.Weapon;
 
 public abstract class Character {
@@ -42,24 +47,31 @@ public abstract class Character {
     protected Animation m_jumpAnimation;
     protected Animation m_runAnimation;
     protected TextureRegion m_currentFrameTexture;
-    protected boolean m_isFacesRight;
 
     protected StateMove m_currentStateMove;
     protected String m_name;
     protected Weapon m_weapon;
     protected int m_life;
 
-    protected Sound m_walkSound;
+    protected Music m_walkSound;
     protected Sound m_standSound;
     protected Sound m_jumpSound;
     protected Sound m_runSound;
+    protected Sound m_groundSound;
 
     protected float m_stateTime;
+    protected boolean m_isFacesRight;
+    protected boolean m_isGrounded;
+    protected Vector2 m_moveVector;
 
-    public Character(Vector2 pos, FileHandle spriteConfigFile, StateMove move, String name, Weapon weapon, int life){
+    protected TiledMapConfig m_mapConfig;
+
+    public Character(Vector2 pos, TiledMapConfig mapConfig, FileHandle spriteConfigFile, StateMove move, String name,
+                     Weapon weapon, int life){
+        m_mapConfig = mapConfig;
         m_spriteTextureConfig = new SpriteTextureConfig(spriteConfigFile);
         m_spriteTexture = m_spriteTextureConfig.getTexture();
-        m_size = m_spriteTextureConfig.getSizeOneCell();
+        m_size = m_spriteTextureConfig.getSizeOneCell().scl(TiledMapConfig.TILE_UNIT_SCALE);
 
         m_position = pos;
         m_currentStateMove = move;
@@ -74,12 +86,29 @@ public abstract class Character {
         m_runAnimation = m_spriteTextureConfig.getRunAnimationConfig().getAnimation();
         m_jumpAnimation = m_spriteTextureConfig.getJumpAnimationConfig().getAnimation();
 
+        m_walkSound = Gdx.audio.newMusic(Gdx.files.internal("sound/player/walk.mp3"));
         m_stateTime = 0;
+        m_moveVector = new Vector2(0,0);
+        m_isGrounded = false;
+
+        m_weapon.setCharacter(this);
+    }
+
+    public Rectangle getTiledCollided(Vector2 start, Vector2 end, TiledMapTileLayer collisionLayer){
+        for(int x = (int)start.x; x<=end.x; ++x ){
+            for(int y = (int)start.y ; y<=end.y ; ++y){
+                if(collisionLayer.getCell(x,y) != null)
+                    return new Rectangle(x,y,1,1); // 1 == 1unit
+            }
+        }
+        return null;
     }
 
     public Rectangle getRectangle(){
         return new Rectangle(m_position.x, m_position.y, m_size.x, m_size.y);
     }
+
+    public abstract void render(float deltaTime, Batch toRender, TiledMapOrthographicCamera camera, TiledMapTileLayer collision);
 
     public void render(float deltaTime, Batch batchToRender){
         m_stateTime += deltaTime;
@@ -102,6 +131,84 @@ public abstract class Character {
             batchToRender.draw(m_currentFrameTexture, m_position.x, m_position.y, m_size.x, m_size.y);
         else
             batchToRender.draw(m_currentFrameTexture, m_position.x + m_size.x, m_position.y, -m_size.x, m_size.y);
+    }
+
+    public abstract void update(TiledMapTileLayer collisionLayer);
+
+    public Vector2 getPosition() {
+        return m_position;
+    }
+
+    public Vector2 getSize() {
+        return m_size;
+    }
+
+    public SpriteTextureConfig getSpriteTextureConfig() {
+        return m_spriteTextureConfig;
+    }
+
+    public Texture getSpriteTexture() {
+        return m_spriteTexture;
+    }
+
+    public Animation getWalkAnimation() {
+        return m_walkAnimation;
+    }
+
+    public Animation getStandAnimation() {
+        return m_standAnimation;
+    }
+
+    public Animation getJumpAnimation() {
+        return m_jumpAnimation;
+    }
+
+    public Animation getRunAnimation() {
+        return m_runAnimation;
+    }
+
+    public TextureRegion getCurrentFrameTexture() {
+        return m_currentFrameTexture;
+    }
+
+    public boolean isFacesRight() {
+        return m_isFacesRight;
+    }
+
+    public StateMove getCurrentStateMove() {
+        return m_currentStateMove;
+    }
+
+    public String getName() {
+        return m_name;
+    }
+
+    public Weapon getWeapon() {
+        return m_weapon;
+    }
+
+    public int getLife() {
+        return m_life;
+    }
+
+    public Music getWalkSound() {
+        return m_walkSound;
+    }
+
+    public Sound getStandSound() {
+        return m_standSound;
+    }
+
+    public Sound getJumpSound() {
+        return m_jumpSound;
+    }
+
+    public Sound getRunSound() {
+        return m_runSound;
+    }
+
+    public float getStateTime() {
+        return m_stateTime;
     }
 
     public void dispose(){
--- core/src/fr/plafogaj/game/character/player/Player.java
+++ core/src/fr/plafogaj/game/character/player/Player.java
@@ -15,56 +15,138 @@
 
 package fr.plafogaj.game.character.player;
 
-import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import fr.plafogaj.game.character.Character;
 import fr.plafogaj.game.character.StateMove;
+import fr.plafogaj.game.engine.TiledMapConfig;
+import fr.plafogaj.game.engine.TiledMapOrthographicCamera;
 import fr.plafogaj.game.weapon.Weapon;
+import fr.plafogaj.game.weapon.longRange.Arc;
+import fr.plafogaj.game.weapon.longRange.LongRange;
+import fr.plafogaj.game.weapon.shortRange.Sword;
 
-public class Player extends Character implements InputProcessor{
+public class Player extends Character{
+    private static float MAX_VELOCITY = 0.1f;
+    private static float GRAVITY= -0.004f;
+    private Armor m_armor;
 
-    public Player(Vector2 pos, FileHandle spriteConfigFile, StateMove move, String name, Weapon weapon, int life){
-        super(pos,spriteConfigFile,move,name,weapon,life);
+    public Player(Vector2 pos, TiledMapConfig mapConfig, FileHandle spriteConfigFile, StateMove move, String name, Weapon weapon, int life){
+        super(pos, mapConfig, spriteConfigFile, move, name, weapon, life);
+    }
+
+    public Player(Vector2 pos, TiledMapConfig mapConfig){
+        this(pos, mapConfig, Gdx.files.internal("img/sprite/player/config.json"), StateMove.Walking, "Player", new Arc(), 10);
     }
 
     @Override
-    public boolean keyDown(int keycode) {
-        return false;
+    public void render(float deltaTime, Batch toRender, TiledMapOrthographicCamera camera, TiledMapTileLayer collision){
+        this.update(collision);
+        super.render(deltaTime, toRender);
+        m_weapon.update(m_position);
+        m_weapon.render(deltaTime, toRender, camera, collision);
     }
 
     @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
+    public void update(TiledMapTileLayer collisionLayer){
 
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
+        // weapon
+        if(m_weapon instanceof LongRange){
+            if(Gdx.input.isKeyPressed(Input.Keys.U)){
+                ((LongRange) m_weapon).upAngle();
+            }
 
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
+            if(Gdx.input.isKeyPressed(Input.Keys.I)){
+                ((LongRange) m_weapon).downAngle();
+            }
+        }
 
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
 
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
+        if(Gdx.input.isKeyPressed(Input.Keys.H))
+            m_weapon.hit();
 
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
+        //movement
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.RIGHT) ||
+           Gdx.input.isKeyPressed(Input.Keys.Q) || Gdx.input.isKeyPressed(Input.Keys.D)){
+            if(m_isGrounded)
+                m_currentStateMove = StateMove.Walking;
+            m_isFacesRight = (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D));
+            m_moveVector.x = Player.MAX_VELOCITY *(m_isFacesRight ? 1 : -1);
+        }
+        else{
+            m_moveVector.x = 0;
+            m_currentStateMove = StateMove.Standing;
+        }
 
-    @Override
-    public boolean scrolled(int amount) {
-        return false;
+        if(Gdx.input.isKeyPressed(Input.Keys.SPACE) && m_isGrounded){
+            m_currentStateMove = StateMove.Jumping;
+            m_moveVector.y = Player.MAX_VELOCITY;
+            m_isGrounded = false;
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)){
+            if(m_isGrounded){
+                m_currentStateMove = StateMove.Running;
+                m_moveVector.scl(1.8f);
+            }
+        }
+
+
+        m_moveVector.y += Player.GRAVITY;
+
+        //collision
+        Vector2 start = new Vector2(), end = new Vector2();
+        Rectangle tmpCollide;
+
+        end.x = start.x = m_position.x + m_moveVector.x;
+        if(m_moveVector.x >0)
+            end.x = (start.x += m_size.x);
+        start.y = m_position.y+0.2f;
+        end.y = start.y + m_size.y;
+
+        if(this.getTiledCollided(start, end, collisionLayer) == null)
+            m_position.x += m_moveVector.x;
+
+        start.x = m_position.x;
+        end.x = m_position.x + m_size.x;
+        start.y = end.y = m_position.y + m_moveVector.y;
+        if(m_moveVector.y > 0)
+            start.y = (end.y += m_size.y);
+
+        tmpCollide = this.getTiledCollided(start, end, collisionLayer);
+
+        if(tmpCollide != null){
+            if(m_moveVector.y > 0 ){
+                m_position.y = tmpCollide.y - m_size.y;
+            }
+            else{
+                m_position.y = tmpCollide.y + 1;
+                m_isGrounded = true;
+            }
+            m_moveVector.y = 0;
+        }
+
+        m_position.y += m_moveVector.y;
+        if(m_position.y<0) {
+            m_isGrounded = true;
+            m_position.y = 0;
+            m_life = 0;
+        }
+        if(m_position.x <0)
+            m_position.x = 0;
+
+//        if(m_isGrounded){
+//            if((int)m_moveVector.x == 0)
+//                m_currentStateMove = StateMove.Standing;
+//            else
+//                m_currentStateMove = StateMove.Walking;
+//        }
+//        else
+//            m_currentStateMove = StateMove.Jumping;
     }
 }
